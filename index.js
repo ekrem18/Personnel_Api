ac4d8df01c594f4228e05a705f29d7175c2e9c06
Ekrem <avekremyilmazturk@gmail.com>
2023-10-10 21:23:09 +0300

    skeleton

--- index.js
+++ index.js
@@ -12,10 +12,18 @@ const express = require('express')
 const app = express()
 
 /* ------------------------------------------------------- */
+//Required Modules:
+require('dotenv').config()
+const PORT = process.env?.PORT || 8000
+
+require('express-async-errors')
+/* ------------------------------------------------------- */
+//Configurations
 
 
 
-// continue from here...
+
+
 
 
 

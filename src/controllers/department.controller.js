6366d352905f413e5a42bbf6c056406d2fe0e4e6
Ekrem <avekremyilmazturk@gmail.com>
2023-10-10 21:50:51 +0300

    controller

--- src/controllers/department.controller.js
+++ src/controllers/department.controller.js
@@ -1,4 +1,26 @@
 "use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
+/*------------------------------------------------------- */
+const Department = require('../models/department.model')
+
+module.exports={
+    list: async (req, res)=>{
+        const data = await res.getModelList(Department)
+
+    },
+
+    create: async (req, res)=>{
+
+    },
+
+    read: async (req, res)=>{
+
+    },
+
+    update: async (req, res)=>{
+
+    },
+
+    delete: async (req, res)=>{
+
+    },
+}

f2c1928b2a46046296231c5b35439dfe3e724531
Ekrem <avekremyilmazturk@gmail.com>
2023-10-12 13:56:44 +0300

    personnel part

--- src/routes/personnel.router.js
+++ src/routes/personnel.router.js
@@ -2,6 +2,21 @@
 
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+const personnel = require('../controllers/personnel.controller')
+
+router.route('/')
+    .get(personnel.list)
+    .post(personnel.create)
+
+router.route('/:id')
+    .get(personnel.read)
+    .put(personnel.update)
+    .patch(personnel.update)
+    .delete(personnel.delete)
+
+
+
+
 
 
 

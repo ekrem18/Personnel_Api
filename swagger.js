65d8294ebe4ef08a7abf27b92d07aa81cfbe0aef
Ekrem <avekremyilmazturk@gmail.com>
2023-10-16 14:20:43 +0300

    swagger

--- swagger.js
+++ swagger.js
@@ -1,3 +1,77 @@
 "use strict"
-//npm i swagger-autogen
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+require('dotenv').config()
+const HOST = process.env?.HOST || '127.0.0.1'
+const PORT = process.env?.PORT || 8000
+/* ------------------------------------------------------- */
+// npm i swagger-autogen
+// const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' })
+const swaggerAutogen = require('swagger-autogen')()
+const packageJson = require('./package.json')
 
+const document = {
+	// info: {
+	// 	version: "1.0.0",
+	// 	title: "Personnel API",
+	// 	description: "Personnel Management API Service",
+	// 	termsOfService: "http://www.clarusway.com",
+	// 	contact: { name: "Clarusway", email: "qadir@clarusway.com" },
+	// 	license: { name: "BSD License", },
+	// },
+	info: {
+		version: packageJson.version,
+		title: packageJson.title,
+		description: packageJson.description,
+		termsOfService: "http://www.clarusway.com",
+		contact: { name: packageJson.author, email: "qadir@clarusway.com" },
+		license: { name: packageJson.license, },
+	},
+	host: `${HOST}:${PORT}`,
+	basePath: '/',
+	schemes: ['http', 'https'],
+	// JWT Settings:
+	securityDefinitions: {
+		JWT: {
+			type: 'apiKey',
+			in: 'header',
+			name: 'Authorization',
+			description: 'Entry Your AccessToken (JWT) for Login. Example: <b>Bearer <i>...token...<i></b>'
+		}
+	},
+	security: [{ "JWT": true }],
+	definition: {
+		"/auth/login": {
+			username: {
+				type: "String",
+				required: true
+			},
+			password: {
+				type: "String",
+				required: true
+			},
+		},
+		"/auth/refresh": {
+			"token.refresh": {
+				description: "{ token: { refresh: ... } }",
+				type: "String",
+				required: true
+			}
+		},
+		// "Department": {
+		// 	"name": {
+		// 		type: "ObjectId",
+		// 		required: true
+		// 	}
+		// },
+		"Department": require('./src/models/department.model').schema.obj,
+		"Personnel": require('./src/models/personnel.model').schema.obj,
+	}
+};
+
+const routes = ['./index.js']
+const outputFile = './swagger.json'
+
+// Create JSON file:
+swaggerAutogen(outputFile, routes, document)
